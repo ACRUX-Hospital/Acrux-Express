240fbb137b73e2ca3c0fb2fc9d42832f65d506a9
asemnouri <asem_nori@hotmail.com>
2020-12-16 17:11:41 +0200

    merge

--- client/App.js
+++ client/App.js
@@ -3,15 +3,18 @@ import { StyleSheet, View, Text } from 'react-native';
 // import { NativeRouter, Route, Link } from "react-router-native";
 // import {Login}  from './components/pages/login'
 // import {Sigup}  from './components/pages/signUp'
- import Home  from './components/pages/home'
+//  import Home  from './components/pages/home'
 // import {Chat}  from './components/pages/chat'
-import Navigator from './routes/homeStack';
+// import Navigator from './routes/homeStack';
 
 export default function App() {
   return (
     // <Home/>
-    <Navigator/>
-    
+    // <Navigator/>
+    <View>
+   <Text 
+      style={{fontSize:35,marginLeft:18,marginTop:10,color:"#3b3b3b"}}>welcome to</Text>
+    </View>
   );
 }
 
