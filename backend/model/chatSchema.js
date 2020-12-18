bc1e970be47cabe3954fb470dd8289af7f9319b2
asemnouri <asem_nori@hotmail.com>
2020-12-18 19:05:15 +0200

    chat react-native

--- backend/model/chatSchema.js
+++ backend/model/chatSchema.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose');
 
 const chatSchema = mongoose.Schema({
     roomID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Room'
+        type:String
     },
-    UserID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+    UserName: {
+        type: String
     },
     message: {
         type: String
@@ -15,7 +13,19 @@ const chatSchema = mongoose.Schema({
 },{ timestamps: true })
 
 
-const Chat = mongoose.model('department', chatSchema);
+const Chat = mongoose.model('chatmessage', chatSchema);
 
 
 module.exports = { Chat };
+
+
+//get room
+//get the room  from db(check if it is there or not) and create it if no room
+//populate the messages if the is room
+
+//add the message to db
+//will give you back an id ==> put the id in the room model(first find if there is a room or not)
+
+//retrieve all messages
+//get the room name from front-end
+//populate by messages
--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ mongoose.connect(process.env.DB_CONNECT,
 
 const express = require('express')
 const app = express()
-var cors = require('cors')
+var cors = require('cors');
+const { Chatroom, } = require('./model/chatRoomSchema');
+const {Chat}=require("./model/chatSchema")
 const server = require('http').createServer(app);
 
  
@@ -42,15 +44,29 @@ io.on('connection', (socket) => {
 
 
   console.log(`New connection ! + ${socket.id}`)
-  socket.on('join', ({ room }) => {
+  socket.on('join', async({ room }) => {
     console.log(room)
     socket.join(room)
+    let dbRoom=await Chatroom.findOne({roomName:room}).populate('messages')
+    if(!dbRoom){
+      let newRoom=new Chatroom({roomName:room})
+      await newRoom.save()
+
+    }else{
+      // let roomMessages= await dbRoom.populate('messages')
+      socket.emit("data",  {messages:dbRoom.messages} )
+    }
     console.log(`joined at ${room}`)
-    socket.emit("data", { "asem": "hello" })
   })
-  socket.on('messages', ({ message, room }) => { // to get the data from front emit function
+
+
+  socket.on('messages', async({ message, room }) => { // to get the data from front emit function
     console.log(message, room)
-    io.to(room).emit('newMessage', { message })
+    let chat =new Chat({roomID:room,UserName:"asem",message})
+    let chatObj=await chat.save()
+    let roomName=await Chatroom.findOne({roomName:room})
+    await Chatroom.updateOne({_id:roomName._id},{messages:[...roomName.messages,chatObj._id]})
+    io.to(room).emit('newMessage', chat)
   })
   socket.on('disconnect', () => {
     console.log('user has left')
