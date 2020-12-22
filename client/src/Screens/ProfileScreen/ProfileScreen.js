4256d5639c771575bdecb2bc21e33181df19663a
israakittaneh <israakittanehm@gmail.com>
2020-12-22 08:51:14 +0200

    profile

--- client/src/Screens/ProfileScreen/ProfileScreen.js
+++ client/src/Screens/ProfileScreen/ProfileScreen.js
@@ -1,12 +1,92 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// import React from 'react'
+// import { View, Text } from 'react-native'
 
-const ProfileScreen = () => {
+// const ProfileScreen = () => {
+//     return (
+//         <View>
+//             <Text> Profile Page </Text>
+//         </View>
+//     )
+// }
+
+// export default ProfileScreen
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+export default class Profile extends Component {
+
+  render() {
     return (
-        <View>
-            <Text> Profile Page </Text>
+      <View style={styles.container}>
+          <View style={styles.header}></View>
+          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>Iman Kittaneh</Text>
+              <Text style={styles.info}>iman@gmail.com</Text>
+              <Text style={styles.description}>Phone Number</Text>
+              
+                         
+              {/* <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 2</Text> 
+              </TouchableOpacity> */}
+            </View>
         </View>
-    )
+      </View>
+    );
+  }
 }
 
-export default ProfileScreen
+const styles = StyleSheet.create({
+  header:{
+    backgroundColor: "#00BFFF",
+    height:200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:130
+  },
+  name:{
+    fontSize:22,
+    color:"#FFFFFF",
+    fontWeight:'600',
+  },
+  body:{
+    marginTop:40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding:30,
+  },
+  name:{
+    fontSize:28,
+    color: "#696969",
+    fontWeight: "600"
+  },
+  info:{
+    fontSize:16,
+    color: "#00BFFF",
+    marginTop:10
+  },
+  description:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10,
+    textAlign: 'center'
+  },
+  
+});
